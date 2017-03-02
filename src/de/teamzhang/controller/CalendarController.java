5ebfe730fef9a5e69b8ce19a7bf80e43efaca8a9
Lucas <l.solcher@gmail.com>
2017-03-02 15:23:11 +0100

    schlingel du :)

--- src/de/teamzhang/controller/CalendarController.java
+++ src/de/teamzhang/controller/CalendarController.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.mvc.AbstractController;
 // @RequestMapping("/public")
 public class CalendarController extends AbstractController {
 
-	@RequestMapping("/")
+	@RequestMapping("/calendar")
 	protected ModelAndView handleRequestInternal(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
 
 		ModelAndView modelandview = new ModelAndView("calendar");
