f5b639b38366698c57b0daeecf5b696cabf4f720
Lucas <l.solcher@gmail.com>
2017-04-04 15:21:51 +0200

    added post for json

--- src/de/teamzhang/controller/CalendarController.java
+++ src/de/teamzhang/controller/CalendarController.java
@@ -4,8 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
@@ -26,5 +28,12 @@ public class CalendarController extends AbstractController {
 	protected String getVeranstaltungen() {
 		// TODO read from veranstaltungen.json
 		return "[{\"stg_name\": \"Angewandte Informatik (B)\",\"veranstaltungsart\": \"Übung\",\"parallelgruppe\": \"\",\"semester\": \"20162\",\"rhythmus\": \"Einzeltermin\",\"kurzname\": \"DeO1Ws\",\"sws\": \"4\"}]";
+	}
+
+	@RequestMapping(value = "/calendar/post", method = RequestMethod.POST)
+	public @ResponseBody void updateData(@RequestBody String jsonString) {
+
+		System.out.println(jsonString);
+
 	}
 }
