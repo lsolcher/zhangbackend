12f96d0d2c2434c1867b4535ab0b5ebee7f8b299
Lucas <s0539248@htw-berlin.de>
2017-04-05 00:46:26 +0200

    read json from veranstaltungen.json

--- src/de/teamzhang/controller/CalendarController.java
+++ src/de/teamzhang/controller/CalendarController.java
@@ -1,8 +1,13 @@
 package de.teamzhang.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,19 +20,33 @@ import org.springframework.web.servlet.mvc.AbstractController;
 public class CalendarController extends AbstractController {
 
 	@RequestMapping(value = "/calendar", method = RequestMethod.GET)
-	protected ModelAndView handleRequestInternal(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse arg1)
+			throws Exception {
 
 		ModelAndView modelandview = new ModelAndView("calendar");
 		// modelandview.addObject("welcomeMessage", "Hi User");
-		modelandview.addObject("veranstaltungen", getVeranstaltungen());
+		modelandview.addObject("veranstaltungen", getVeranstaltungen(request));
 
 		return modelandview;
 
 	}
 
-	protected String getVeranstaltungen() {
+	protected String getVeranstaltungen(HttpServletRequest request) {
 		// TODO read from veranstaltungen.json
-		return "[{\"stg_name\": \"Angewandte Informatik (B)\",\"veranstaltungsart\": \"Übung\",\"parallelgruppe\": \"\",\"semester\": \"20162\",\"rhythmus\": \"Einzeltermin\",\"kurzname\": \"DeO1Ws\",\"sws\": \"4\"}]";
+		JSONParser parser = new JSONParser();
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(request.getSession().getServletContext()
+					.getResourceAsStream("/resources/json/veranstaltungen.json")));
+			Object obj = parser.parse(reader);
+			JSONArray array = new JSONArray();
+			array.add(obj);
+			return array.toString();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+		//return "[{\"stg_name\": \"Angewandte Informatik (B)\",\"veranstaltungsart\": \"Übung\",\"parallelgruppe\": \"\",\"semester\": \"20162\",\"rhythmus\": \"Einzeltermin\",\"kurzname\": \"DeO1Ws\",\"sws\": \"4\"}]";
 	}
 
 	@RequestMapping(value = "/post", method = RequestMethod.POST)
