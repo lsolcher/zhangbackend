1d3b98cf3029aea500c8186b83aa606356cc80ad
Lucas <l.solcher@gmail.com>
2017-05-11 12:51:41 +0200

    mapping update

--- src/de/teamzhang/model/SimplePrio.java
+++ src/de/teamzhang/model/SimplePrio.java
@@ -5,7 +5,8 @@ import java.math.BigInteger;
 public class SimplePrio extends Prio {
 
 	boolean acceptsFourHourCourses;
-
+	int option;
+	
 	public SimplePrio() {
 
 		// TODO Auto-generated constructor stub
@@ -14,6 +15,10 @@ public class SimplePrio extends Prio {
 	public SimplePrio(String string, BigInteger profID, BigInteger[] courses, boolean acceptsFourHourCourses) {
 		super(string, profID, courses);
 		this.acceptsFourHourCourses = acceptsFourHourCourses;
+	}
+	
+	public void setOption(int option) {
+		this.option =option;
 	}
 
 }
