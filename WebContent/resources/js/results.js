893a0ac30414dcdc6b79ad8d46cfddd76902d7e2
Dorette <s0541685@htw-berlin.de>
2017-06-22 15:01:32 +0200

    trying to solve file reading problem

--- WebContent/resources/js/results.js
+++ WebContent/resources/js/results.js
@@ -2,64 +2,81 @@
 $(document).ready(function(){
 	
 	var singleElements = [];
+	var semesterID = "";
+	var timeID = "";
+	var fullTableID = "";
 	
-	$.ajax({
-		type: "GET",
-		url: "Teacher2.csv",
-		dataType: "csv",
-		success: function(data) {processData(data);}
-	});
+	for (var semesterIMIba = 1; semesterIMIba < 6; semesterIMIba++) {
 	
-//	var semester = 0;
-	var kurs = "xxx";
-	var dozent = "yyy";
-	var raum = "000";
-	
-	for (var semester = 0; semester <= 9; semester++) {
-	// TODO: html-tabelle befüllen: document.getElementById('table-cell-' + i + semester).innerHTML = arrayInhalt... ;
-		for (var i = 0; i < 35; i++) {
-		//for (var i = 0; i < singleElements.length; i++) {	
+		$.ajax({
+			type: "GET",
+			url: "Bachelor IMI " + semesterIMIba + ".csv",
+			dataType: "csv",
+			success: function(data) {processData(data);}
+		});
 		
-			if (singleElements[i] != 0 ) {
-				
-//				kurs = ;
-//				dozent = ;
-//				raum = ;
-				
-				document.getElementById('table-cell-' + i + semester).innerHTML = "Kurs: " + kurs + ", Dozent: " + dozent + ", Raum: " + raum;
-			}
-		}
+//		semesterID = "" + (semesterIMIba - 1 ); //TODO .toString
+//			
+//		for (var i = 0; i < 35; i++) {
+//		//for (var i = 0; i < singleElements.length; i++) {	
+//						
+//			timeID = "";
+//			fullTableID = "table-cell-" + timeID + semesterID;
+//			
+//			if (singleElements[i] != null ) {
+//				
+//				console.log("null");
+////				document.getElementById(fullTableID).innerHTML = ;
+//				
+//				
+//				
+////				document.getElementById(fullTableID).innerHTML = "Kurs: " + kurs + ", Dozent: " + dozent + ", Raum: " + raum;
+//			}
+//		}
+		
+	
 	}
-	
-	
+	for (var semesterIMIma = 1; semesterIMIma < 4; semesterIMIma++) {
+		semesterID = "" + (semesterIMIma + 5 );  //  + 6 - 1 //TODO .toString
+		timeID = "";
+		fullTableID = "table-cell-" + timeID + semesterID;
+		
+		
+		
+		
+		
+		
+	}
 	
 	
 });
 
 function processData(allText) {
 	
-	alert("-> " + allText);
-	
 	var allTextLines = allText.split(/\r\n|\n/);
-	singleElements = allText.split("\n|','");
+//	singleElements = allText.split("\n|';'");
 	
 	alert("-> " + allTextLines[0]);
 	
 	
-	for (var i = 0; i < singleElements.length; i++) {
-		alert("entry: " + singleElements[i]);
-	}
+//	for (var i = 0; i < singleElements.length; i++) {
+//		alert("entry: " + singleElements[i]);
+//	}
 	
+//	for (var i = 1; i < allTextLines.length; i++) {
+//		singleElements.push(allTextLines[i].split(','));
+//		alert((allTextLines[i].split(',')));
+//	}
 
 	//function processData(allText) {
 //	    var allTextLines = allText.split(/\r\n|\n/);
-//	    var headers = allTextLines[0].split(',');
+//	    var headers = allTextLines[0].split(';');
 //	    var lines = [];
 	//    
 //	    console.log("in!");
 	//
 //	    for (var i=1; i<allTextLines.length; i++) {
-//	        var data = allTextLines[i].split(',');
+//	        var data = allTextLines[i].split(';');
 //	        if (data.length == headers.length) {
 	//
 //	            var tarr = [];
@@ -72,11 +89,4 @@ function processData(allText) {
 //	    // alert(lines);
 	//}
 
-	
-	/*var singleElements = [];
-	
-	for (var i = 1; i < allTextLines.length; i++) {
-		singleElements.push(allTextLines[i].split(','));
-		alert((allTextLines[i].split(',')));
-	}*/
 }
