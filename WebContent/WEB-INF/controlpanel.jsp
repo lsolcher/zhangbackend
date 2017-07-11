5c0fe1e032b971ac2e9d9b08819a01cf5106af53
Ollie <ollie@ollies-mbp.speedport_w_723v_1_43_000>
2017-07-11 17:30:18 +0200

    controlpanel cleanup

--- WebContent/WEB-INF/controlpanel.jsp
+++ WebContent/WEB-INF/controlpanel.jsp
@@ -19,7 +19,8 @@
   	<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
   	<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.js"></script>
   	<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.0rc1/angular-route.min.js"></script>
-  	<spring:url var ="courses" value="/resources/js/courses.js" />
+    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <spring:url var ="courses" value="/resources/js/courses.js" />
   	<script type="text/javascript" src="${courses}"></script>
   	<spring:url var ="additionalPriorities" value="/resources/js/additionalPriorities.js" />
     <script type="text/javascript" src="${additionalPriorities}"></script>
--- WebContent/resources/js/additionalPriorities.js
+++ WebContent/resources/js/additionalPriorities.js
@@ -1,75 +1,38 @@
 (function() {
   var app = angular.module("zhang-app").controller('additionalPrioController', function($scope, $rootScope) {
 
-      $scope.selectPrio = function(index, prio) {
+    $rootScope.additionalPriorities = [];
 
-          for(var i in $rootScope.additionalPriorities) {
-              $rootScope.additionalPriorities[i].hideContent = true;
-          }
+    $scope.selectPrio = function(index, prio) {
 
-          var newPrio = jQuery.extend(true, {}, prio);
-          newPrio.origin = index;
-
-          $rootScope.additionalPriorities.push(newPrio);
-
+      for(var i in $rootScope.additionalPriorities) {
+          $rootScope.additionalPriorities[i].hideContent = true;
       }
 
-      $rootScope.additionalPriorities = [];
-      // $rootScope.additionalPriorities = [
-      //     {
-      //         type: 'additionalPrio',
-      //         title: 'MaxStunden',
-      //         options: '3',
-      //         text: 'Maximale Stunden pro Tag: ',
-      //         prio: 'hoch',
-      //         program: 'IMI-B'
-      //     },
-      //     {
-      //         type: 'additionalPrio',
-      //         title: 'MaxMinuspunkte',
-      //         options: '2',
-      //         text: 'Maximale Stunden pro Tag: ',
-      //         prio: 'niedrig',
-      //         program: 'IMI-B'
-      //     },
-      //     {
-      //         type: 'additionalPrio',
-      //         title: 'MaxPausen',
-      //         options: '1',
-      //         text: 'Maximale Stunden pro Tag: ',
-      //         prio: 'mittel',
-      //         program: 'IMI-B'
-      //     }
-      // ];
-      // $scope.selectPrio = function(index, prio) {
-      //     var newPrio = jQuery.extend(true, {}, prio);
-      //     newPrio.origin = index;
-      //     $scope.additionalPriorities.push(newPrio);
-      // }
+      var newPrio = jQuery.extend(true, {}, prio);
+      newPrio.origin = index;
 
-      // $scope.change = function(selected) {
-      //     console.log(selected);
-      //     $scope.additionalPriorities.options = selected;
-      // }
+      $rootScope.additionalPriorities.push(newPrio);
 
-    // TODO: When Checkbox "checked" add Object to $rootScope.additionalPriorities
+    }
 
     $scope.save = function() {
 
         console.log($rootScope.additionalPriorities);
-	      $.ajax({
-	        type: 'POST',
-	        contentType : 'application/json; charset=utf-8',
-	        url: '/ZhangProjectBackend/postConfig.json',
-	        data: JSON.stringify($rootScope.additionalPriorities),
-	        success: function(response) {
-	          console.log('Responese', response);
-	          alert('Die Daten wurden erfolgreich in der Datenbank gespeichert.');
-	        },
-	        error: function(response) {
-	          console.error('Response', response);
-	        }
-	      });
+          $.ajax({
+            type: 'POST',
+            contentType : 'application/json; charset=utf-8',
+            url: '/ZhangProjectBackend/postConfig.json',
+            data: JSON.stringify($rootScope.additionalPriorities),
+            success: function(response) {
+              console.log('Responese', response);
+              toastr.success('Die Daten wurden erfolgreich in der Datenbank gespeichert.');
+            },
+            error: function(response) {
+              console.error('Response', response);
+              toastr.error('Die Daten wurden leider nicht in der Datenbank gespeichert.');
+            }
+          });
     }
 
     $scope.possibleAdditionalPriorities = [
@@ -127,16 +90,16 @@
   });
 
 
-      angular.module("zhang-app").directive('AddPriority', function($scope) {
-          return function(scope, element, attrs) {
+    angular.module("zhang-app").directive('AddPriority', function($scope) {
+      return function(scope, element, attrs) {
 
-              scope.changeOption = function(selected) {
-                  scope.prio.option = selected;
-              }
-              scope.changePrio = function(selected) {
-                  scope.prio.prio = selected;
-              }
-
+          scope.changeOption = function(selected) {
+              scope.prio.option = selected;
           }
-      });
-  })();
+          scope.changePrio = function(selected) {
+              scope.prio.prio = selected;
+          }
+
+      }
+    });
+})();
