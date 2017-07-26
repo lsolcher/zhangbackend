71b9013a63b91cc6840b38534a1e513bd32c06f3
Lucas <s0539248@htw-berlin.de>
2017-07-26 21:02:17 +0200

    fixed login.reload bug

--- src/de/teamzhang/controller/LogoutController.java
+++ src/de/teamzhang/controller/LogoutController.java
@@ -7,16 +7,20 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class LogoutController {
 
 	@RequestMapping(value = "/logout", method = RequestMethod.GET)
-	public String logout(HttpServletRequest request) {
+	public ModelAndView logout(HttpServletRequest request) {
 		HttpSession httpSession = request.getSession();
 		SecurityContextHolder.getContext().setAuthentication(null);
 		httpSession.invalidate();
-		return "index";
+		ModelAndView modelandview = new ModelAndView(new RedirectView("index.html"));
+
+		return modelandview;
 	}
 
 }
